f3c7a25118b17a27cf788512dba9fde7ff6ae07a
Danny Nguyen <nguyenduongpa@gmail.com>
2023-10-09 08:23:40 -0700

    d

--- bitcoin.py
+++ bitcoin.py
@@ -144,18 +144,12 @@ def scalar_mul(x, y, times):
         print('{}*(x,y) = ({}, {})'.format(s, result.x.num, result.y.num))
 
 
-gx = 0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798
-gy = 0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8
-p = 2**256 - 2**32 - 977
-
-n = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
-
-x = FieldElement(gx, p)
-y = FieldElement(gy, p)
-seven = FieldElement(7, p)
-zero = FieldElement(0, p)
-G = Point(x, y, zero, seven)
-print(n*G)
+# x = FieldElement(gx, P)
+# y = FieldElement(gy, P)
+# seven = FieldElement(7, P)
+# zero = FieldElement(0, P)
+# G = Point(x, y, zero, seven)
+# print(n*G)
 
 
 class S256Field(FieldElement):
@@ -166,10 +160,6 @@ class S256Field(FieldElement):
         return '{:x}'.format(self.num).zfill(64)
 
 
-A = 0
-B = 7
-
-
 class S256Point(Point):
     def __init__(self, x, y, a=None, b=None):
         a, b = S256Field(A), S256Field(B)
@@ -177,3 +167,16 @@ class S256Point(Point):
             super().__init__(x=S256Field(x), y=S256Field(y), a=a, b=b)
         else:
             super().__init__(x=x, y=y, a=a, b=b)
+
+    def __rmul__(self, coefficient):
+        coef = coefficient % N
+        return super().__rmul__(coef)
+
+
+A = 0
+B = 7
+P = 2**256 - 2**32 - 977
+N = 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141
+G = S256Point(
+    0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,
+    0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8)
